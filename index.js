5df143e3ec206cf6b45e7efcca3a4e69ab9992ce
eduardyt959-wq <eduardyt959@gmail.com>
2026-06-07 17:46:23 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log('Hello!');
+
+window.alert('This is a useless website.')
+
+window.alert('This is still a useless website.')
+
+window.alert('Why are you even here?')
+
+function ShowMessage() {
+    alert("I am currently still a useless website forever.");
+}
+
+/*
+this is a useless website but now the devs can see it.
+*/
